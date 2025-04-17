d7b338924b0e69041e47c972bde0583c7193fa0c
FrankFeitosa <feitosfrank22@gmail.com>
2025-04-17 10:26:22 -0300

    adiciona novos metodos na api

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ app.use(express.json())
 
 //Banco de dados Fake (em memória)
 const usuarios = [
-    {id: 1, nome: "João", email:"joao@emaill.com"},
-    {id: 2, nome: "Ana", email:"ana@emaill.com"}
+    { id: 1, nome: "João", email: "joao@emaill.com" },
+    { id: 2, nome: "Ana", email: "ana@emaill.com" }
 ]
+// mostra o que tem no servidor
+app.get("/usuarios", (req, res) => {
+    res.send(`
+        Estes são todos os usuários cadastrados: 
+         ${JSON.stringify(usuarios)}
+        `)
+});
 
-app.put("/usuario/:id", (req, res)=>{
+//Criar novo usuário
+app.post("/criarUsuario", (req, res) => {
+    const { nome, email } = req.body
+    usuarios.push({
+        id: usuarios[usuarios.length - 1].id + 1,
+        nome: nome,
+        email: email
+    })
+    res.send(usuarios)
+});
+
+// Atualiza os dados do usuario no servidor fake
+app.put("/usuario/:id", (req, res) => {
     const { id } = req.params
     const { novoNome, novoEmail } = req.body
     const usuario = usuarios.find(
@@ -23,18 +42,16 @@ app.put("/usuario/:id", (req, res)=>{
 
     res.send(id)
 })
-
-app.get("/usuarios", (req,res) => {
-    res.send(`
-        Estes são todos os usuários cadastrados: 
-         ${JSON.stringify(usuarios)}
-        `)
-});
-
-app.post("/criarUsuario", (req,res) => {
-    const {id, nome, email} = req.body
-    res.send(usuarios.push({id, nome, email}))
-});
+// deleta usuario
+app.delete("/usuario/:id", (req, res) => {
+    const { id } = req.params
+    const index = usuarios.findIndex((usuario) => {return usuario.id == id})
+    if(index === -1){
+        res.send("Usuário não encontrado")
+    }
+    usuarios.splice(index, 1)
+    res.send(usuarios)
+})
 
 app.listen(port, () => {
     console.log(`
